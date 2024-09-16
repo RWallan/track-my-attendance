79f0304c16d7d3bfcb6a871108638ce6a7593068
RWallan <3am.richardwallan@gmail.com>
2024-09-16 19:06:52 -0300

    refac: moving security tests

--- backend/tests/test_security.py
+++ backend/tests/test_security.py
@@ -38,3 +38,41 @@ def test_decode_jwt():
 
     assert token['test'] == data['test']
     assert token['exp']
+
+
+def test_update_user_with_invalid_token(client):
+    response = client.put(
+        '/users/me',
+        json={'name': 'Updated User', 'password': 'newpassword'},
+        headers={'Authorization': 'Bearer 123'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_update_user_with_invalid_user(client, token, session, user):
+    session.delete(user)
+    session.commit()
+
+    response = client.put(
+        '/users/me',
+        json={'name': 'Updated User', 'password': 'newpassword'},
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_update_user_with_wrong_token(client):
+    wrong_token = JWT.encode({'sub': ''})
+
+    response = client.put(
+        '/users/me',
+        json={'name': 'Updated User'},
+        headers={'Authorization': f'Bearer {wrong_token}'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
